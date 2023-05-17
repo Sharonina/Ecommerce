a2d0c4ab9b6a81652477e8dcdcded2199a8b8f46
jeannie alvarado <jeaniie38@gmail.com>
2023-05-17 12:45:48 -0600

    tarjetas de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import ProductCard from './products/ProductCard';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+  
+  function App() {
+    const [products, setProducts] = useState([]);
+  
+    useEffect(() => {
+      fetch("https://dummyjson.com/products?limit=100")
+        .then((res) => res.json())
+        .then((data) => {
+          setProducts(data.products);
+        });
+    }, []);
+  
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          images={product.images}
+          title={product.title}
+          price={product.price}
+        />
+      ))}
+    </div>
+  );
+  }
+
+
 
 export default App
